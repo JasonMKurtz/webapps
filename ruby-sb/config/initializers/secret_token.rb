d212b779d77fdd59a9a6e511f03c25e24faa5aa7
JasonMKurtz <jason@jkurtz.net>
2012-11-26 22:28:02 -0500

    create a ruby sandbox

---
+++ ruby-sb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubySb::Application.config.secret_token = 'a3e11d73100228d069e46f014132da0c7dcb4d5033997b930805265fd145e7948fa1a3ea96f2139571817a7bebbc7504c0c2a225746ff564c2edde30ddc0e9b4'
