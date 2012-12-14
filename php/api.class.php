6601f0ae5cfc9e1ad87f1f08037f509f7fd3d365
Jason Kurtz <jason@jkurtz.net>
2012-12-14 00:46:27 -0500

    API::SetOpt() now will accept an associative array or a key/value pair as its params, API::GetOpts() will return any non-default curl options that have been set, API::GetData() will now return data as an array or as an object, specified by a constructor parameter.

--- php/api.class.php
+++ php/api.class.php
@@ -1,15 +1,19 @@
 <?php
 	class API { 
-		private $url, $type, $curl; 
+		private $url, $type, $curl, $curlopts, $datatype; 
 		private $data = array(), $reply; 
 
-		function __construct($url, $data = "", $type = "POST") {
+		// receiveData signifies the type of data that will be returned. 
+		// 0 = associative array, 1 = object
+		function __construct($url, $receiveData = 1, $data = "", $type = "POST") {
 			if ($data != "") { 
 				$this->data = $this->FormatData($data); 
 			}
 			$this->url      = $url; 
 			$this->type     = strtoupper($type); 			
 			$this->curl	= curl_init($this->url); 
+			$this->curlopts = array(); 
+			$this->datatype = $receiveData; 
 			return $this;
 		}
 
@@ -27,9 +31,31 @@
 
 
 
-		function SetOpt($opt, $value) { 
-			curl_setopt($this->curl, $opt, $value); 
-		} 
+		function SetOpt() {
+			$args = func_get_args();  
+			if (!isset($args[1]) && !is_array($args[0])) 
+				return; 
+
+			if (is_array($args[0])) {
+				foreach ($args[0] as $key => $value) {
+					$lkey = floatval($key); 
+					if (!isset($this->curlopts[$key])) {
+						curl_setopt($this->curl, $lkey, $value); 
+						$this->curlopts[$key] = $value; 
+					}
+				}
+			} else {
+				if (!isset($this->curlopts[$args[0]])) {
+					$larg0 = floatval($args[0]); 
+					curl_setopt($this->curl, $larg0, $args[1]);
+					$this->curlopts[$args[0]] = $args[1]; 
+				} 
+			} 
+		}
+
+		function GetOpts() { 
+			return $this->curlopts; 
+		}			 
 
 		function Send($data = "") { 
 			if ($this->data == "") {
@@ -57,7 +83,7 @@
 				return $decode;
 			else {  
 				if (is_object($decode)) 
-					return (array) $decode; 
+					return ($this->datatype == 1 ? $decode : (array) $decode); 
 
 				// if we haven't returned by now, it's a string, that might be spliced by &'s
 				// we can make an assoc array out of that, just like our data. 
@@ -66,7 +92,7 @@
 					$data = explode("=", $data); 
 					$da[$data[0]] = $data[1]; 
 				}
-				return $da; 
+				return ($this->datatype == 1 ? (object) $da : $da); 
 			} 
 		}
 	}		
