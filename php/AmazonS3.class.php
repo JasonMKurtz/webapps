ce16f39cd66ff36a4ef46df2ad096939a8af222a
Jason Kurtz <jason@jkurtz.net>
2013-07-20 23:15:24 -0400

    Amazon S3 API wrapper

---
+++ php/AmazonS3.class.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php
+<?php
+	require 'vendor/autoload.php'; 
+	require 's3.conf.php'; 
+	use Aws\S3\S3Client;
+		
+	class AmazonS3 {
+		private $client, $bucket; 
+		
+		function __construct($bucket) { 
+			$this->client = S3Client::factory(array('key' => S3Key, 'secret' => S3Secret)); 
+			if (!$this->client->doesBucketExist($bucket)) { 
+				die(sprintf("Bucket %s does not exist. Aborting.\n", $bucket)); 
+			}
+			$this->bucket = $bucket; 
+		}
+
+		function doUpload($file, $name, $verbose = 0, $public = 'public-read') { 
+			if (!is_file($file)) { 
+				die(sprintf("File %s does not exist.\n", $file)); 
+			}
+
+			try {
+				$this->client->upload($this->bucket, $name, fopen($file, 'r'), $public); 
+			} catch (S3Exception $e) { 
+				die(sprintf("Couldn't upload %s to %s. Aborting.\n", $file, $this->bucket)); 
+			}
+			if ($verbose) { 
+				return sprintf("Upload complete. %s (saved as %s) to %s bucket.", $file, $name, $this->bucket); 
+			} else {
+				return sprintf("https://s3.amazonaws.com/%s/%s", $this->bucket, $name); 
+			}
+		}
+	}
+
+	$S3 = new AmazonS3("b1.aws"); 
+	echo $S3->doUpload("terminals.png", "terminals.png"); 
+?> 
