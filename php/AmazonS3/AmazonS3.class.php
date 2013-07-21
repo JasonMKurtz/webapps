28b8472fcf81ac7b4f22b4d9220201c0052a8c87
Jason Kurtz <jason@jkurtz.net>
2013-07-21 00:53:54 -0400

    Added GenRand() function to create a obsfucated filename when uploading, if one isn't specified.

--- php/AmazonS3/AmazonS3.class.php
+++ php/AmazonS3/AmazonS3.class.php
@@ -20,8 +20,10 @@
 				die(sprintf("File %s does not exist.\n", $file)); 
 			}
 
+			$ext = pathinfo($file, PATHINFO_EXTENSION); 
+
 			if ($name == -1) 
-				$name = $file; 
+				$name = sprintf("%s.%s", $this->GenRand(25), $ext); 
 
 			try {
 				$this->client->upload($this->bucket, $name, fopen($file, 'r'), $public); 
@@ -33,6 +35,43 @@
 			} else {
 				return sprintf("https://s3.amazonaws.com/%s/%s", $this->bucket, $name); 
 			}
+		}
+
+		private function GenRand($len = 10) {
+			if (@is_readable('/dev/urandom')) {
+				$f=fopen('/dev/urandom', 'r');
+				$urandom=fread($f, $len);
+				fclose($f);
+			}
+
+			$value='';
+
+			for ($i=0;$i<$len;++$i) {
+
+				if (!isset($urandom)) {
+					if ($i%2==0) {
+						mt_srand(time()%2147 * 1000000 + (double)microtime() * 1000000);
+					}
+
+					$rand=48+mt_rand()%64;
+				} else {
+					$rand=48+ord($urandom[$i])%64;
+				}
+
+				if ($rand>57)
+					$rand+=7;
+				if ($rand>90)
+					$rand+=6;
+
+				if ($rand==123)
+					$rand=45;
+				if ($rand==124)
+					$rand=46;
+
+				$value.=chr($rand);
+			}
+
+			return $value;
 		}
 	}
 
